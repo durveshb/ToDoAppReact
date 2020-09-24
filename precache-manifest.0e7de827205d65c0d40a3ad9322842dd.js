d55d882550611057d4b3cef21e3c388fe3ad9622
Durvesh Bhalekar <durvesh.5.db@gmail.com>
2020-09-24 18:17:36 +0530

    Updates

---
+++ precache-manifest.0e7de827205d65c0d40a3ad9322842dd.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29252e58baf987c5a6084c79a0676528",
+    "url": "/ToDoAppReact/index.html"
+  },
+  {
+    "revision": "fdd81af4dde163f2e7d7",
+    "url": "/ToDoAppReact/static/css/main.af6f372c.chunk.css"
+  },
+  {
+    "revision": "f8073f1ab84776896058",
+    "url": "/ToDoAppReact/static/js/2.4a77b871.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDoAppReact/static/js/2.4a77b871.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdd81af4dde163f2e7d7",
+    "url": "/ToDoAppReact/static/js/main.a2db59de.chunk.js"
+  },
+  {
+    "revision": "b30c63b2afa17c061672",
+    "url": "/ToDoAppReact/static/js/runtime-main.c503ee20.js"
+  },
+  {
+    "revision": "a2641a96db8949b9f8b866e94388ac8e",
+    "url": "/ToDoAppReact/static/media/bg.a2641a96.png"
+  },
+  {
+    "revision": "ea00a6a93e5941419a81e0e396edc870",
+    "url": "/ToDoAppReact/static/media/catAcademic.ea00a6a9.svg"
+  },
+  {
+    "revision": "59f3d012236c5560c92ac87479dcde73",
+    "url": "/ToDoAppReact/static/media/catPersonal.59f3d012.svg"
+  },
+  {
+    "revision": "2c039af28b81725aa77156427dfb7691",
+    "url": "/ToDoAppReact/static/media/catSocial.2c039af2.svg"
+  },
+  {
+    "revision": "6d1f12600dccc10b21be651f557f40d4",
+    "url": "/ToDoAppReact/static/media/detailbg.6d1f1260.png"
+  },
+  {
+    "revision": "be7fc76a15110ace5ef6479ec44ebf2e",
+    "url": "/ToDoAppReact/static/media/headerbg.be7fc76a.png"
+  },
+  {
+    "revision": "ddd50b75b299b3e6090bd9a192be83f3",
+    "url": "/ToDoAppReact/static/media/urgHigh.ddd50b75.svg"
+  },
+  {
+    "revision": "ebcb8ed530d11c62d2f700ce506364e1",
+    "url": "/ToDoAppReact/static/media/urgLow.ebcb8ed5.svg"
+  },
+  {
+    "revision": "765851308817dd96ea392055bc0b5d91",
+    "url": "/ToDoAppReact/static/media/urgMedium.76585130.svg"
+  }
+]);
